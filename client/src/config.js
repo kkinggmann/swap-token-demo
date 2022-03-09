a00ea9f7260181593299f45c482b8dd9849d1d72
longcn <nngoclongg1999@gmail.com>
2022-03-09 14:23:34 +0700

    Deploy reactjs

---
+++ client/src/config.js
@@ -0,0 +1,3 @@
+export const tokenAAddress = "0x964105A777A79606Ab6D3BA5e6B3082d6B3e5BCd";
+export const tokenBAddress = "0xa0b7150F3eFF5ABEfb9d78e41968fdBe2aD01a2f";
+export const tokenPoolAddress = "0x963E35633508910F2cf622B033732DB267c16021";
